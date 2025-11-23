4183791d820fb040b14f14601af941f023d160d7
time2024 <1940876394@qq.com>
2025-11-23 21:54:47 +0800

    Site updated: 2025-11-23 21:54:47

--- document/js-parser/tests/negative/asi_ambiguous.js
+++
@@ -1,6 +0,0 @@
-// Cases where ASI doesn't help
-a = b + c
-(d + e).print()  // Interpreted as: a = b + c(d + e).print()
-
-var foo = "bar"
-["red", "green"].foreach(...)  // Interpreted as: foo["red", "green"]
